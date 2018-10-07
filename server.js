ca1216699a4d4fcfe195ac045d0f92d88ad4ad5b
Gautham Lal <gautham1995@gmail.com>
2018-10-07 20:20:57 +0530

    Added project page to website and project link to head

--- server.js
+++ server.js
@@ -69,6 +69,12 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/project', (req, res) => {
+  res.render('project.hbs', {
+    pageTitle: 'Project Page!'
+  });
+});
+
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage: 'Bad server request'
